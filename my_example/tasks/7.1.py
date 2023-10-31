7dc5efa4868b6ae0be1bb0d71be0480a6b13cbad
Pelevin Andrey <dr.proton@yandex.ru>
2023-10-31 23:36:06 +0300

    Python: Done task 7.1

--- my_example/tasks/7.1.py
+++ my_example/tasks/7.1.py
@@ -1,5 +1,5 @@
-lst = []
+lst = {}
 with open('ospf.txt', 'r') as f:
-    lst.append(f)
+    for item in f:
+        print(f"Pref{item.split()[1]:>32}\nAD/Mertic{item.split()[2].strip('[]'):>21}\nNext-Hop{item.split()[4].strip(','):>25}\nLast update{item.split()[5].strip(','):>18}\nOutbound Interface{item.split()[6]:>21}\n")
 
-print(lst)
