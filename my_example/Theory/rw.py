15186e409a9eb7edb33b5d5dc316a6121ae44fa6
Pelevin Andrey <dr.proton@yandex.ru>
2023-10-31 21:27:15 +0300

    Python: Write file

--- my_example/Theory/rw.py
+++ my_example/Theory/rw.py
@@ -1,7 +1,26 @@
-f = open("my_example/Theory/test1.txt", "r")
+# f = open("my_example/Theory/test1.txt", "r")
 # print(f.read())
 # print(f.readline())
 # for item in f:
 #     print(item)
 # print(f.readlines())
-print(f.read().split("\n"))
+# print(f.read().split("\n"))
+
+cfg_lines = ['!',
+'service timestamps debug datetime msec localtime show-timezone year',
+'service timestamps log datetime msec localtime show-timezone year',
+'service password-encryption',
+'service sequence-numbers',
+'!',
+'no ip domain lookup',
+'!',
+'ip ssh version 2',
+'!']
+
+file1 = "my_example/Theory/r2.txt"
+
+f = open(file1, 'w')
+str1 = "\n".join(cfg_lines)
+# f.write(str1)
+f.writelines(str1)
+f.close()
