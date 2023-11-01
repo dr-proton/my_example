bdba9605c868022407509b5ac59da0b41490e265
Pelevin Andrey <dr.proton@yandex.ru>
2023-11-01 23:22:04 +0300

    Change task 7.3a

--- my_example/tasks/7.3a.py
+++ my_example/tasks/7.3a.py
@@ -1,8 +1,6 @@
-lst = []
-
 with open("CAM_table.txt", 'r') as f:
     for i in f:
         if "Gi" in i:
-            lst.append(i.replace("DYNAMIC", '').split())
+            lst = i.replace("DYNAMIC", '').split()
 
 print(lst)
