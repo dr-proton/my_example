c4945d8d783366d984e0b7eadabacd7860eb8559
Pelevin Andrey <dr.proton@yandex.ru>
2023-11-05 08:58:39 +0300

    Python: done 9.1a

---
+++ my_example/Theory/func_param.py
@@ -0,0 +1,7 @@
+lst1 = [1,2,3]
+lst2 = [4,5,6]
+
+def sum(*arg):
+    print(list(arg))
+
+sum(lst1, lst2)
--- my_example/tasks/9.1a.py
+++ my_example/tasks/9.1a.py
@@ -11,3 +11,31 @@ port_security_template = [
 ]
 
 access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+
+def generate_access_config(intf_vlan_mapping, *access_template, psecurity=None):
+    lst = []
+    for k,v in intf_vlan_mapping.items():
+        lst.append(f"Interface {k}")
+        if psecurity:
+            for i in access_template[0]:
+                if "vlan" in i:
+                    lst.append(f"{i} {v}")
+                else:
+                    lst.append(i)
+    
+            for j in access_template[1]:
+                    lst.append(f"{j}")
+        else:
+             for i in access_template[0]:
+                if "vlan" in i:
+                    lst.append(f"{i} {v}")
+                else:
+                    lst.append(i)
+
+    return lst
+
+res1 = generate_access_config(access_config, access_mode_template)
+res2 = generate_access_config(access_config, access_mode_template, port_security_template, psecurity=True)
+
+print("\n".join(res1), "\n")
+print("\n".join(res2))
