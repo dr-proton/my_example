5f5ecfe92843e4215f5e66e2195d62e87e686f8f
Pelevin Andrey <dr.proton@yandex.ru>
2023-11-05 18:28:36 +0300

    Python: done 9.3, start 9.3a

---
+++ my_example/tasks/9.3.py
@@ -0,0 +1,21 @@
+def get_int_vlan_map(config_filename):
+    mode_access = {}
+    mode_trunk = {}
+    lst = []
+
+    with open(config_filename, 'r') as f:
+        for i in f:
+            if "interface" in i:
+                intf = i.split()[-1]
+            elif "trunk allowed" in i:
+                # mode_trunk[intf] = i.split()[-1].split()
+                lst_vl = i.split()[-1].split(',')
+                mode_trunk[intf] = [int(vl) for vl in lst_vl]
+            elif "access vlan" in i:
+                mode_access[intf] = int(i.split()[-1])
+            
+        return (mode_trunk, mode_access)
+
+
+res = get_int_vlan_map("config_sw1.txt")
+print(res[1])
