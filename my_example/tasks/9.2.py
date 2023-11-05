014f562c272a19ee45c9e448b4743f5e411cbc80
Pelevin Andrey <dr.proton@yandex.ru>
2023-11-05 09:35:45 +0300

    Python: start task 9.2

---
+++ my_example/tasks/9.2.py
@@ -0,0 +1,13 @@
+trunk_mode_template = [
+    "switchport mode trunk", "switchport trunk native vlan 999",
+    "switchport trunk allowed vlan"
+]
+
+trunk_config = {
+    "FastEthernet0/1": [10, 20, 30],
+    "FastEthernet0/2": [11, 30],
+    "FastEthernet0/4": [17]
+}
+
+def generate_trunk_config(*arg):
+    pass
