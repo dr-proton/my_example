11f4f7c4e87cfe1b9c1850767b59a3201f568d12
Pelevin Andrey <dr.proton@yandex.ru>
2023-11-07 17:19:33 +0300

    Python: done 9.4

--- my_example/tasks/9.4.py
+++ my_example/tasks/9.4.py
@@ -1,4 +1,35 @@
 ignore = ["duplex", "alias", "Current configuration"]
+dct = {}
 
+def convert_config_to_dict(config_filename):
+    lst = []
+    intf = ""
+    with open(config_filename, 'r') as f:
+        duplex, alias, cur_conf = ignore
+        for i in f:
+            if "!" in i:
+                continue
+            elif duplex in i:
+                continue
+            elif alias in i:
+                continue
+            elif cur_conf in i:
+                continue
+            else:
+                lst.append(i.rstrip())
 
-def ignore_command(command, ignore):
+    for c in range(len(lst)):
+        if lst[c]:
+            if lst[c].startswith(" ") and not lst[c-1].startswith(" "):
+                intf = lst[c-1]
+                dct[intf] = [lst[c].strip()]
+            elif lst[c].startswith(" "):
+                dct[intf].append(lst[c].strip())
+            elif not lst[c].startswith(" ") and not lst[c-1].startswith(" ") and not lst[c+1].startswith(" "):
+                dct[lst[c]] = []
+
+    return dct
+               
+            
+res = convert_config_to_dict("config_sw1.txt")
+print(res)
--- my_example/tasks/strings.py
+++ my_example/tasks/strings.py
@@ -1,12 +1,19 @@
-tunnel = """
-interface Tunnel0
- ip address 10.10.10.1 255.255.255.0
- ip mtu 1416
- ip ospf hello-interval 5
- tunnel source FastEthernet1/0
- tunnel protection ipsec profile DMVPN
-"""
-print(tunnel)
+lst1 = ["1","2","3","4","5"]
+lst2 = ["2","4"]
 
-string1 = 'interface FastEthernet1/0'
-print(string1[1])
+# for i in lst1:
+#     for j in lst2:
+#         if j in i:
+#             print(i)
+
+ignore = ["duplex", "alias", "Current configuration"]
+
+j = 0
+
+for i in lst1:
+    while j < len(lst2):
+        if lst2[j] in i:
+             continue
+        else:
+            print(i)
+        j += 1
